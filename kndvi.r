b4b556af3181ae6f27b87aa0aeae5f8f0dcdc30a
gcampsvalls <gustau.camps@uv.es>
2021-03-07 21:12:19 +0100

    Update kndvi.r

--- kndvi.r
+++ kndvi.r
@@ -1,7 +1,9 @@
 # kNDVI with RBF kernel.
 # nir: near infrared (pixel) value
 # red: red value
-sigma <- 1
+
+# Fix or estimate a reasonable sigma value
+sigma <- 0.15
 knr <- exp(-(nir-red)^2/(2*sigma^2))
 kndvi <- (1-knr) / (1+knr)
 
