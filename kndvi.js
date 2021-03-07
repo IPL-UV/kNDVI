1479b51fba21a533b746078477da270237f9ef8e
gcampsvalls <gustau.camps@uv.es>
2021-03-07 21:10:55 +0100

    Update kndvi.js

--- kndvi.js
+++ kndvi.js
@@ -3,18 +3,17 @@
 // nir and red are near-infrared and red bands, respectively.
 // The example here assumes the values of these bands are *reflectances*.
 // For instace, for MODIS you must divide the raw values by 10000.
-// See the image collection documentation you use for specific instructions.
+// See the image collection documentation for specific instructions.
 
-// We present two options:
-// 1. Using the definition of RBF kernel: exp(-(nir-red)^2/(2*sigma^2))
-// 2. Equivalent: tanh((nir-red)^2/(2*sigma)^2)
+// We present two implementations which are equivalent: 
+//      1. Using the definition of the RBF kernel k(nir,red)=exp(-(nir-red)^2/(2*sigma^2)) in the kNDVI=(1-k(nir,red))/(1+k(nir,red))
+//      2. which is equivalent to kNDVI = tanh((nir-red)^2/(2*sigma)^2)
 // Both obtain the same result, use the one you prefer!
 
 var addKNDVI_RBF = function(image) {
   // Compute (nir-red)^2
   var D2 = nir.subtract(red).pow(2);
-  // Sigma: you have to adjust this for your data.
-  // Here we use a value sensible for MODIS reflectances.
+  // Fix or estimate a reasonable sigma value, e.g. sigma = 0.15
   var sigma = ee.Number(0.15);
   // Compute kernel (k) and kNDVI
   var k = D2.divide(sigma.pow(2)).multiply(-1).exp();
